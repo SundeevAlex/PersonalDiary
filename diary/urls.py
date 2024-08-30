f4c023ea7c2bb3c57263721a01f5b5d3b6bc80d8
Aleksandr Sundeev <navigator97@mail.ru>
2024-08-30 11:11:29 +0700

    Add home.html

---
+++ diary/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from diary.apps import DiaryConfig
+from diary.views import home
+
+app_name = DiaryConfig.name
+
+urlpatterns = [
+    path('', home, name='home'),
+
+]
