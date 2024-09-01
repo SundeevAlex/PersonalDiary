b4e21419d17ceeef2a8a7e65d700cec218b25505
Aleksandr Sundeev <navigator97@mail.ru>
2024-09-01 15:15:22 +0700

    Add diary_detail

--- diary/urls.py
+++ diary/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from diary.apps import DiaryConfig
-from diary.views import diary_list
+from diary.views import diary_list, diary_detail
 
 app_name = DiaryConfig.name
 
 urlpatterns = [
-    path("", diary_list, name="diary_list"),
+    path('', diary_list, name='diary_list'),
+    path('diary/<int:pk>/', diary_detail, name='diary_detail'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from diary.models import Diary
 
@@ -7,3 +7,10 @@ def diary_list(request):
     records = Diary.objects.all()
     context = {"record": records}
     return render(request, "diary/diary_list.html", context)
+
+
+def diary_detail(request, pk):
+    # record = Diary.objects.get(pk=pk)
+    record = get_object_or_404(Diary, pk=pk)
+    context = {"record": record}
+    return render(request, "diary/diary_detail.html", context)
